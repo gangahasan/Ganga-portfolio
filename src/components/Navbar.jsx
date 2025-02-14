feb4f035bdfb904194347240fb976ad4ef4089c7
gangahasan <rgangabhavani2@gmail.com>
2025-02-14 18:46:55 +0530

    added responsiveness to different screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // Function to close menu when clicking a link
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <h1 style={{ color: "rgb(78, 78, 249)", fontSize: "28px" }}>Ganga</h1>
-      <ul className="listitems">
+
+      {/* Hamburger Icon (Only for Small Screens) */}
+      <button className="hamburger" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      {/* Navigation Links */}
+      <ul className={`listitems ${isOpen ? "show" : ""}`}>
         <li>
-          <a href="#home">Home</a>
+          <a href="#home" onClick={closeMenu}>
+            Home
+          </a>
         </li>
         <li>
-          <a href="#about">About Me</a>
+          <a href="#about" onClick={closeMenu}>
+            About Me
+          </a>
         </li>
         <li>
-          <a href="#skills">Skills</a>
+          <a href="#skills" onClick={closeMenu}>
+            Skills
+          </a>
         </li>
         <li>
-          <a href="#projects">Projects</a>
+          <a href="#projects" onClick={closeMenu}>
+            Projects
+          </a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a href="#contact" onClick={closeMenu}>
+            Contact
+          </a>
         </li>
         <li>
-          <a href="#resume">Resume</a>
+          <a href="#resume" onClick={closeMenu}>
+            Resume
+          </a>
         </li>
       </ul>
     </nav>
